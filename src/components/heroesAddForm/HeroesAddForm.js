a9380c0e39a3099fc06334c73f3f843fa5c10e04
Vlad Korenevskiy <vladik-korenevskiy@mail.ru>
2022-12-14 21:56:04 +0300

    first commit

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,93 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import { heroAdded, heroesFetchingError } from '../../actions';
+import { v4 as uuidv4 } from 'uuid';
+
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+    const [heroName, setHeroName] = useState('');
+    const [heroDescr, setHeroDescr] = useState('');
+    const [heroElement, setHeroElement] = useState('');
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    const addHero = (e) => {
+        e.preventDefault();
+        const newHero = {
+            'id': uuidv4(),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement,
+        };
+        
+        request(`http://localhost:5000/heroes`, 'POST', JSON.stringify(newHero))
+        .then(dispatch(heroAdded(newHero)))
+        .catch(() => dispatch(heroesFetchingError()));
+        
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+    }
+
+    return (
+        <form onSubmit={addHero} className="border p-4 shadow-lg rounded">
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    placeholder="Как меня зовут?"
+                    value={heroName}
+                    onChange={(e) => setHeroName(e.target.value)}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="description" 
+                    className="form-control" 
+                    id="text" 
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}
+                    value={heroDescr}
+                    onChange={(e) => setHeroDescr(e.target.value)}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)}>
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
---
+++ src/reducers/index.js
@@ -0,0 +1,46 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+    filters: [],
+    filtersLoadingStatus: 'idle',
+    activeFilter: 'all',
+    filteredHeroes: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        case 'HERO_DELETED':
+            const newHeroList = state.heroes.filter(item => item.id !== action.payload)
+
+            return {
+                ...state,
+                heroes: newHeroList
+            }
+        case 'HERO_ADDED':
+            const newList = [...state.heroes, action.payload];
+            
+            return {
+                ...state,
+                heroes: newList
+            }
+        default: return state
+    }
+}
+
+export default reducer;
